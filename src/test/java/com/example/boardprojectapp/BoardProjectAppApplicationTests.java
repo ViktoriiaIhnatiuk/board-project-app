ea6a5ab7d5d67c5a81a52118d27807fa19e391fd
Viktoriia Ihnatiuk <victoriia.ihnatiuk@gmail.com>
2022-07-20 14:02:24 +0300

    First commit

---
+++ src/test/java/com/example/boardprojectapp/BoardProjectAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boardprojectapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardProjectAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
